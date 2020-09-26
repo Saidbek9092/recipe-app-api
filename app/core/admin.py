7d74943eb0c33e9ddd59d6e59a19d60f2ec71568
Saidbek <saidbek9092@gmail.com>
2020-09-26 21:03:54 +0500

    Updated configurations.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,33 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext as _
-
+# from django.utils.translation import gettext as _
 from core import models
+
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name',)}),
-        (
-            _('Permissions'),
+        (('Personal Info'), {'fields': ('name',)}),
+        (('Permissions'),
             {
             'fields': (
                 'is_active',
                 'is_staff',
-                'is_superuser',
-            )
-        }
-        ),
-        (_('Important dates'), {'fields': ('last_login',)})
-
-    )
+                'is_superuser',)}),
+        (('Important dates'), {'fields': ('last_login',)}))
     add_fieldsets = (
-        (None,{
-            'classes':('wide'),
-            'fields':('email','password1','password2')
-        }),
-    )
+        (None, {
+            'classes': ('wide'),
+            'fields': ('email', 'password1', 'password2')}),)
+
 
 admin.site.register(models.User, UserAdmin)
